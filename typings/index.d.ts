efeabcc8f911cd531632898238c20d42724e246a
tescostum <tescostum@gmail.com>
2016-06-28 23:27:53 +0900

    nodejsの定義追加

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,4 @@
+/// <reference path="globals/node-4/index.d.ts" />
 /// <reference path="globals/react-dom/index.d.ts" />
 /// <reference path="globals/react/index.d.ts" />
 /// <reference path="globals/superagent/index.d.ts" />
