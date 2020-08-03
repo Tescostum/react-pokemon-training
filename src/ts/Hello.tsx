ab552f0ee7883b2f98699f11a97522a064c17064
Tescostum <tescostum@gmail.com>
2020-08-04 01:30:27 +0900

    change message

--- src/ts/Hello.tsx
+++ src/ts/Hello.tsx
@@ -5,6 +5,6 @@ export interface HelloProps { compiler: string; framework: string; }
 
 export class Hello extends React.Component<HelloProps, {}> {
     render() {
-        return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+        return <h1>Hello from {this.props.compiler} and {this.props.framework}.</h1>;
     }
 }
