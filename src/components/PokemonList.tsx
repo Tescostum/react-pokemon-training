93a7037b6982593a10f8106334a2c30c958ba705
tescostum <tescostum@gmail.com>
2020-08-04 02:52:40 +0900

    css定義

--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -1,6 +1,7 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom";
-import {PokemonBox} from "./PokemonBox"
+import {PokemonBox} from "./PokemonBox";
+import "./PokemonList.css";
 
 export interface PokemonListState { idList:number[] }
 
